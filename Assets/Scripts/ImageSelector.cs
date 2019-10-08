ad597aef1166943ec4a60ce30d1cac9ebe6c5862
Michael Lauer <Lauer16915@gmail.com>
2019-10-08 19:26:21 -0400

    Initial Commit
    Includes Oculus Touch Integration. Need to fix gallery raycasting.

---
+++ Assets/Scripts/ImageSelector.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(GraphicRaycaster))]
+
+public class ImageSelector : MonoBehaviour
+{
+    public string categoryTitleName;
+    public Text categoryTitle;
+    public Image fullImage;
+    public Material highlightMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        categoryTitle.text = categoryTitleName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnPointerDown();
+        }
+    }
+
+   public void OnPointerDown()
+    {
+        GraphicRaycaster gr = GetComponent<GraphicRaycaster>();
+        PointerEventData data = new PointerEventData(null);
+        data.position = Input.mousePosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        gr.Raycast(data, results);
+
+        if(results.Count > 0)
+        {
+            // This means we got a hit
+            OnPreviewClick(results[0].gameObject);
+        }
+    }
+    public void OnPreviewClick(GameObject thisButton)
+    {
+        // set the new image based on what they clicked on
+        Image previewImage = thisButton.GetComponent<Image>();
+        if(previewImage != null)
+        {
+            fullImage.sprite = previewImage.sprite;
+            fullImage.type = Image.Type.Simple;
+            fullImage.preserveAspect = true;
+        }
+    }
+    public void OnPointerEnter(Image image)
+    {
+        // when the user's gaze points to an image, highlight gameObject
+        image.material = highlightMaterial;
+        Debug.Log("Called OnPointerEnter");
+    }
+    public void OnPointerExit(Image image)
+    {
+        image.material = null;
+
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public GameObject galleryHolder;
+    public float slideSpeed;
+
+    private OVRTouchpad.TouchEvent touchEvent;
+    private bool isMoving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OVRTouchpad.Create();
+        OVRTouchpad.AddListener(SwipeHandler);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (!isMoving)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                StartCoroutine(SwipeRight(galleryHolder.transform.position.x));
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                StartCoroutine(SwipeLeft(galleryHolder.transform.position.x));
+            }
+        }
+        #endif
+    }
+    void SwipeHandler(OVRTouchpad.TouchEvent button)
+    {
+        if (!isMoving)
+        {
+            if (button == OVRTouchpad.TouchEvent.Left)
+            {
+                StartCoroutine(SwipeLeft(galleryHolder.transform.position.x));
+            }
+            else if (button == OVRTouchpad.TouchEvent.Right)
+            {
+                StartCoroutine(SwipeRight(galleryHolder.transform.position.x));
+            }
+        }
+    }
+    private IEnumerator SwipeRight(float startingXPos)
+    {
+        while (galleryHolder.transform.position.x != startingXPos - 4)
+        {
+            isMoving = true;
+            Vector3 V3R = new Vector3(startingXPos - 4, galleryHolder.transform.position.y, 0f);
+            galleryHolder.transform.position = Vector3.MoveTowards(galleryHolder.transform.position, V3R, slideSpeed * Time.deltaTime);
+            yield return null;
+        }
+        isMoving = false;
+    }
+    private IEnumerator SwipeLeft(float startingXPos)
+    {
+        while (galleryHolder.transform.position.x != startingXPos + 4)
+        {
+            isMoving = true;
+            Vector3 V3L = new Vector3(startingXPos + 4, galleryHolder.transform.position.y, 0f);
+            galleryHolder.transform.position = Vector3.MoveTowards(galleryHolder.transform.position, V3L, slideSpeed * Time.deltaTime);
+            yield return null;
+        }
+        isMoving = false;
+    }
+}
